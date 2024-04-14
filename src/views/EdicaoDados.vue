24d53d60c87b2fdb06b0405ca3fd0bf427226c76
nb-marques <nb-marques@hotmail.com>
2024-04-14 10:23:02 -0300

    properly commented EdicaoDados.vue

--- src/views/EdicaoDados.vue
+++ src/views/EdicaoDados.vue
@@ -1,12 +1,17 @@
 <template>
   <div>
+    <!-- Formulário para inserir ou editar um item -->
     <b-form class="mt-2">
       <b-row>
+        <!-- Loop através de cada campo do formulário -->
         <b-col class="col-6 offset-sm-3" v-for="(value, key) in form" :key="key">
+          <!-- Grupo de formulário para cada campo -->
           <b-form-group :label="key + ':'" :label-for="key">
+            <!-- Campo de entrada para cada campo -->
             <b-form-input :id="key" v-model="form[key]" type="text" required></b-form-input>
           </b-form-group>
         </b-col>
+        <!-- Botões para salvar e limpar o formulário -->
         <b-col class="col-6 offset-sm-3">
           <b-button type="button" variant="info" class="mr-2" @click="saveTask">Salvar</b-button>
           <b-button type="reset" variant="warning">Limpar</b-button>
@@ -17,19 +22,22 @@
 </template>
 
 <script>
+// Importando a instância do aplicativo Firebase
 import { app } from '../firebaseDB';
+// Importando as funções do Firebase que serão usadas abaixo
 import { getDatabase, ref, set, update } from "firebase/database";
 import Vue from "vue"  
 
 export default {
   data() {
     return {
-      form: {},
-      databaseRef: '', // Adicione a referência ao banco de dados aqui
+      form: {}, // Objeto para armazenar os dados do formulário
+      databaseRef: '', // A referência ao banco de dados
     };
   },
 
   watch: {
+    // Quando o item da rota muda, atualiza o formulário
     '$route.params.item': {
       immediate: true,
       handler(item) {
@@ -38,6 +46,7 @@ export default {
         }
       },
     },
+    // Quando a referência ao banco de dados da rota muda, atualiza a referência ao banco de dados
     '$route.params.databaseRef': {
       immediate: true,
       handler(databaseRef) {
@@ -48,13 +57,14 @@ export default {
     },
   },
 
-
   methods: {
+    // Salva o item no banco de dados
     saveTask() {
+      // Obtém a instância do banco de dados
       const database = getDatabase(app);
 
       if (this.$route.params.item) {
-        // Atualiza o item existente
+        // Se um item foi passado pela rota, atualiza o item existente
         update(ref(database, this.databaseRef + '/' + this.$route.params.item.id), this.form)
           .then(() => {
             this.clearForm();
@@ -65,7 +75,7 @@ export default {
             console.error("Erro ao atualizar item:", error);
           });
       } else {
-        // Criar um novo item
+        // Se nenhum item foi passado pela rota, cria um novo item
         set(ref(database, this.databaseRef + '/' + Date.now()), this.form)
           .then(() => {
             this.clearForm();
@@ -78,10 +88,12 @@ export default {
       }
     },
 
+    // Limpa o formulário
     clearForm() {
       this.form = {};
     },
 
+    // Exibe um toast de sucesso
     makeToast() {
       const vm = new Vue();
       vm.$bvToast.toast('Dados salvos com sucesso', {
