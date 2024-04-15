1dee6b6a3b2d44c10042859ca3d5c2e6736aba7f
unlimitedabe <unlimitedabe@gmail.com>
2024-04-15 20:48:23 -0300

    Adicionado informações e aba "Minicursos" da JCPOLI 2

---
+++ src/models/miniCourses2JCPOLI.ts
@@ -0,0 +1,115 @@
+// Minicursos que estavam na página 2 JCPOLI
+
+let Courses = [
+   
+    {
+      title: "Aplicações em Engenharia utilizando o software Smath Solver",
+      instructor: "Prof. Me. Alberto Vilela Chaer",
+      institution: "PUC Goiás",
+      desc: `O minicurso “Aplicações em Engenharia utilizando o software Smath Solver” tem como objetivo apresentar as possibilidades que este programa – gratuito a partir de download em página oficial na web (https://en.smath.com/view/SMathStudio/summary) – oferece para elaboração de cálculos e geração da respectiva memória de cálculo de um problema qualquer que tenha sua resolução a partir de utilização de expressões matemáticas.  Trata-se de um programa bastante robusto e amplamente conhecido pelo nome de “SMath Studio” utilizado em universidades do Brasil e exterior, sendo uma ferramenta de sedimentação do processo ensino-aprendizagem, através da simulação dos eventos envolvidos em um cálculo. O benefício da utilização deste programa é utilizar da rapidez dos cálculos feitos por meio da máquina, cujas operações e lógica são conduzidas pelo desenvolvimento do usuário, para que se possa tomar conclusões e decisões a partir dos resultados. Uma ferramenta que se constitui em um “caderno de exercícios” de Professores, Estudantes e Profissionais de Engenharia. Recomenda-se que os participantes do referido minicurso acessem a sessão por meio de computador pessoal (sistema operacional Windows e Linux; também por versão Open in browser) para melhor rendimento durante a preleção e atividades propostas, apesar de que o software também tem sua versão gratuita para ser instalada em aparelho celular no sistema Android. `,
+      date: "09/05",
+      time: "14:00-18:00",
+      place: "Microsoft Teams",
+      link: "https://teams.microsoft.com/dl/launcher/launcher.html?url=%2F_%23%2Fl%2Fmeetup-join%2F19%3Ameeting_MWUzMzEwYjQtOTliZC00Zjc0LTk5NGItMmM1NWZmNzM2YTdk%40thread.v2%2F0%3Fcontext%3D%257b%2522Tid%2522%253a%252273319f42-8908-4b89-9f8d-558cf4d5d776%2522%252c%2522Oid%2522%253a%25224d5d6768-2ee9-4adc-ac86-4da09deed785%2522%257d%26anon%3Dtrue&type=meetup-join&deeplinkId=d5ded907-37d1-4a3c-b2f5-6ac85819b99e&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true"
+    }, 
+    {
+      title: "Ferramentas computacionais para o desenho de projeto de arquitetura",
+      instructor: "Alessandro Miguel de Oliveira",
+      institution: "PUC Goiás",
+      desc: `Uso do AutoCAD para o desenho de projeto, geometria descritiva, elementos constitutivos das edificações e representação arquitetônica.`,
+      date: "08, 09 e 10/05",
+      time: "14:00-17:00",
+      place: 'Presencial: Laboratóro 102, Bloco B, Área III'
+    
+    /*  link: ""
+      */
+    },
+  
+    {
+      title: "Introdução a Python",
+      instructor: 'Eugenio José Moura Neto',
+      institution: 'Aluno da PUC Goiás.',
+      desc: `Iniciação à linguagem de programação Python. Vagas limitadas a 20 alunos.`,
+      date: '09/05',
+      time: '14:00-16:00',
+      place: 'Presencial: Laboratório 101, bloco C, área II'
+    
+    },
+  
+    {
+      title: "Introdução ao Git/GitHub",
+      instructor: 'Milton de Assis Ribeiro Neto',
+      institution: 'PUC Goiás',
+      desc: `Como usar o Git/Github? Minicurso presencial, porém com gravação pelo Teams.`,
+      date: '10/05',
+      time: '17:00-19:00',
+      place: 'Presencial: Laboratório 101, bloco C, área II e Transmissão pelo Teams',
+      link: "https://teams.microsoft.com/l/meetup-join/19%3ameeting_Zjk4YTIwZGYtYjRhMC00NGQ5LTk4ZGQtODhiYmVkNGRlZGJk%40thread.v2/0?context=%7b%22Tid%22%3a%2273319f42-8908-4b89-9f8d-558cf4d5d776%22%2c%22Oid%22%3a%224d5d6768-2ee9-4adc-ac86-4da09deed785%22%7d"
+    
+    },
+    
+    {
+      title: "Introdução a Estrutura de Dados em Python",
+      instructor: 'Daniel Ribeiro de Brito',
+      institution: 'Aluno da PUC Goiás',
+      desc: `Este minicurso é uma continuação do curso de Introdução a Python. Serão vistas estruturas de dados simples utilizando a linguagem Python.`,
+      date: '10/05',
+      time: '14:00-16:00',
+      place: 'Presencial: Laboratório 105, bloco C, área II'
+  
+    },
+  ]
+  
+  /*
+  const Courses_Civil = [
+  ]
+  
+  const Courses_Computacao = [
+    
+  
+  
+  ]
+  
+  const Courses_Producao = [
+    
+  ]
+  
+  const Courses_Caer = [
+    
+  ]
+  
+  */
+  Courses = Courses.sort((a, b) => {
+    if(a.date < b.date) return -1;
+    if(a.date > b.date) return 1;
+    if(a.time < b.time) return -1;
+    if(a.time > b.time) return 1;
+    if(a.title < b.title) return -1;
+    if(a.title > b.title) return 1;
+    return 0;
+  });
+  
+  export const miniCourses_Section = [
+    {
+      curso: "Agronomia",
+      minicurso: Courses,
+    },
+    /*
+    {
+      curso: 'Ciências Aeronáuticas',
+      minicurso: Courses_Caer
+    },
+    {
+      curso: "Civil",
+      minicurso: Courses_Civil
+    },
+    {
+      curso: "Computacao",
+      minicurso: Courses_Computacao
+    },
+    {
+      curso: "Producao",
+      minicurso: Courses_Producao
+    },
+    */
+  ]
--- src/views/MiniCoursesj.vue
+++ src/views/MiniCoursesj.vue
@@ -1,155 +1,51 @@
-<!-- Pagina da JCPOLI -  Pagina de Minicuros -->
-<!-- Mantive a mesma o mesmo padrão de css que já estava aqui na Escola Politecna e de Artes, mas os minicursos são as que estavam na JCPOLI -->
 <template>
-    <div>
-      <PhotoHeader
-        :title="title"
-        :description="description"
-        :image="background"
-        offset="calc((-100/523)*100vw + 67.304015296px)"
-      ></PhotoHeader>
-  
-      <Main>
-        <h4 class="text-center">Agenda</h4>
-  
-        <div class="button-container">
-          <button class="button" v-on:click="clearFilter">Todos as datas</button>
-          <button class="button" v-on:click="filterCourses">
-            09/05
-          </button>
-          <button class="button" v-on:click="filterCourses">10/05</button>
-          <button class="button" v-on:click="filterCourses">11/05</button>
-          <!--<button class="button" v-on:click="filterCourses">12/05</button>
-          <button class="button" v-on:click="filterCourses">13/05</button>-->
-        </div>
-        <div class="courses-list">
-          <div v-for="(props, index) in filtered_courses" :key="index">
-            <!--<h3 class="section-course">{{ props.curso }}</h3> -->
-            <div v-for="(course, index) in props.minicurso" :key="index">
-              <MiniCourse :course="course"></MiniCourse>
-              <hr />
-            </div>
-          </div>
-        </div>
-      </Main>
-    </div>
-  </template>
-  
-  <script lang="ts">
-  import { Component, Vue, Prop } from 'vue-property-decorator'
-  import PhotoHeader from '../components/organization/PhotoHeader.vue'
-  import Main from '../components/organization/Main.vue'
-  import MiniCourse from '../components/miniCourse/index.vue'
+  <v-app class="home" id="inspire">
+    <!-- 1º Edição da JCPOLI -->
+    <template v-if="edicaoAtual === 1">
+      <MinicursosJ1 />
+    </template>
+    <!-- 2º Edição da JCPOLI -->
+    <template v-else-if="edicaoAtual === 2">
+      <MinicursosJ2 />
+    </template>
+    <!-- 3º Edição da JCPOLI -->
+    <template v-else-if="edicaoAtual === 3">
+      <MinicursosJ3 />
+    </template>
+   
+   
+  </v-app>
+</template>
 
-  //Para pegar os minicursos que estavam na Escola Politécnica e de Artes
-  //import { miniCourses_Section } from '@/models/miniCourses'
-  
-  //Para pegar os minicursos que estavam na 1 JCPOLI
-  import { miniCourses_Section } from '@/models/miniCourses1JCPOLI'
-  
-  @Component({
-    components: {
-      PhotoHeader,
-      Main,
-      MiniCourse
-    }
-  })
-  export default class MiniCourses extends Vue {
-    private coursesList: any
-    private filtered_courses: any
-  
-    private title = 'Minicursos'
-    private description = 'Minicursos da jornada'
-    private background = '/assets/img/lab.jpg'
-  
-    filterCourses(e: any) {
-      let data = e.target.innerText
-      this.filtered_courses = this.coursesList.map(item => {
-        const minicurso = item.minicurso.filter(aux => {
-          return aux.date == data
-        })
-  
-        return { ...item, minicurso }
-      })
-    }
-  
-    clearFilter() {
-      this.filtered_courses = this.coursesList
-    }
-  
-    constructor() {
-      super()
-  
-      this.coursesList = miniCourses_Section
-      this.filtered_courses = this.coursesList
-    }
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import MinicursosJ1 from '@/components/abasJCPOLI/MinicursosJ1.vue' // 1º Edição da JCPOLI
+import MinicursosJ2 from '@/components/abasJCPOLI/MinicursosJ2.vue' // 2º Edição da JCPOLI
+import MinicursosJ3 from '@/components/abasJCPOLI/MinicursosJ3.vue' // 3º Edição da JCPOLI
+
+//import { speakers_jcpoli1 } from '@/storage/programacao/speakers1JCPOLI'
+
+@Component({
+  components: {
+    MinicursosJ1,
+    MinicursosJ2,
+    MinicursosJ3
   }
-  </script>
-  
-  <style scoped>
-  .courses-list > div:not(:first-child) {
-    margin-top: 35px;
+})
+export default class Jcpoli extends Vue {
+
+  // Para indentificar atraves do link em qual versão da JCPOLI tá
+  get edicaoAtual() {
+    // Obtém o caminho completo da rota atual
+    const caminho = this.$route.fullPath;
+    // Usa expressão regular para extrair o número da edição da rota
+    const match = caminho.match(/\/JCPOLI(\d+)/);
+    // Se houver uma correspondência, retorna o número da edição, caso contrário, retorna null
+    return match ? parseInt(match[1]) : null;
   }
-  
-  .section-course {
-    font-weight: 700;
-    font-size: 2rem;
-    text-align: center;
-    margin-bottom: 2rem;
-    background-color: whitesmoke;
-    box-shadow: 0 0.46875rem 2.1875rem rgba(0, 0, 0, 0.03),
-      0 0.9375rem 1.40625rem rgba(0, 0, 0, 0.03),
-      0 0.25rem 0.53125rem rgba(0, 0, 0, 0.05),
-      0 0.125rem 0.1875rem rgba(0, 0, 0, 0.03);
-    padding: 1rem;
-  }
-  .button-container {
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    margin-bottom: 5rem;
-  }
-  
-  .button {
-    padding: 1rem;
-    border: 0.1rem solid white;
-    border-radius: 5rem;
-    font-weight: 700;
-    background-color: #006ca3;
-    margin-right: 2rem;
-    color: white;
-    transition: all 0.2s ease-in-out;
-  }
-  
-  .button:hover {
-    background-color: #20407d;
-    transform: background-color;
-    transform: scale(1.1);
-  }
-  
-  .button:focus {
-    background-color: #20407d;
-    transform: background-color;
-    transform: scale(1.1);
-  }
-  
-  @media screen and (max-width: 576px) {
-    .button-container {
-      flex-direction: column;
-    }
-    .button {
-      margin-bottom: 1rem;
-    }
-  }
-  
-  @media screen and (min-width: 800px) {
-    .button {
-      width: 10.5rem;
-    }
-  }
-  
-  h4 {
-    color: black;
-    margin-bottom: 3rem;
-  }
-  </style>
+
+}
+</script>
+
+<style>
+</style>
